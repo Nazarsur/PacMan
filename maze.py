a30611457c9f1752e80f6102fe475a7eba5d97f3
Nazarsur <126807144+Nazarsur@users.noreply.github.com>
2023-05-04 19:34:04 +0300

    Add files via upload
    я додав фрукти і змінив лабіринт та скіни сін

--- maze.py
+++ maze.py
@@ -1,6 +1,6 @@
 from pygame import *
 
-WIDTH,HEIGHT = 700, 525  
+WIDTH,HEIGHT = 880, 525  
 FPS = 60
 
 mixer.init()
@@ -27,6 +27,7 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update(self):
+        old_pos = self.rect.x, self.rect.y  
         pressed = key.get_pressed()
         if pressed[K_UP] and self.rect.y > 0:
             self.rect.y -= 3
@@ -36,7 +37,11 @@ class Player(GameSprite):
             self.rect.x -= 3
         if pressed[K_RIGHT] and self.rect.x < WIDTH - 70:
             self.rect.x += 3
-    
+        for w in walls:
+            if sprite.collide_rect(self, w):
+                self.rect.x, self.rect.y = old_pos
+
+
 class Enemy(GameSprite):
     def __init__(self, x, y, speed=3):
         super().__init__("Ghost.png", x, y, 30, 30)
@@ -52,12 +57,17 @@ class Enemy(GameSprite):
       
 class Wall(GameSprite):
     def __init__(self, x, y):
-        super().__init__("background_PM.jpg", x, y, width = 35, height = 35)
+        super().__init__("Wallpm.jpg", x, y, width = 35, height = 35)
 
 
 bg = transform.scale(image.load("background_PM.jpg"), (WIDTH, HEIGHT))
 player = Player("PacManSprite.png", x = 40, y = 350, width = 30, height = 30)
 
+class Fruit(GameSprite):
+    def __init__(self, x, y):
+        super().__init__("fruit.png", x, y, width = 30, height = 30)
+
+fruits = []
 
 walls = []
 enemys = []
@@ -75,8 +85,8 @@ with open('map.txt', 'r') as file:
                 
             elif symbol == "E":
                 enemys.append(Enemy(x,y))
-            elif symbol == '.':
-                coins.append(Coins(x,y))
+            elif symbol == 'o':
+                fruits.append(Fruit(x,y))
                 
  
             x+=35
@@ -102,6 +112,13 @@ while run:
 
         for wall in walls:
             wall.draw()
+
+        for fruit in fruits:
+            fruit.draw()
+
+            if sprite.collide_rect(player, fruit):
+                fruits.remove(fruit)
+
             
 
         for e in enemys:
@@ -111,12 +128,12 @@ while run:
                 result = font1.render("YOU LOSE!", True, (238, 255, 148))
                 window.blit(result, (250,200))
                 finish = True
-                money.play()
+                
 
             
 
         player.draw()
 
-
+        
     display.update()
     clock.tick(FPS)
